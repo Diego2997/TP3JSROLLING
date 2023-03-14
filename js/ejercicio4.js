aa68229d03edb8f950a1db2edc5d2d3f0be855ab
Diego Mercado <diego.mercado1997@hotmail.com>
2023-03-14 09:17:51 -0300

    ternario

--- js/ejercicio4.js
+++ js/ejercicio4.js
@@ -1,11 +1,8 @@
 // Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar. Mostrar por pantalla el resultado devuelto por la función.
 
-const parOImpar = (numero) => {
-  if (numero % 2 === 0) {
-    return document.write(`El numero ${numero} es par`);
-  } else {
-    return document.write(`El numero ${numero} es impar`);
-  }
-};
+const parOImpar = (numero) =>
+  numero === 2
+    ? document.write(`El ${numero} es par`)
+    : document.write(`El ${numero} es impar`);
 
-parOImpar(19);
+parOImpar(8);
