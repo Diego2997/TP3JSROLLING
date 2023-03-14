5aa4b22bcf39ff148bc7ebe46ac67196dfbfe8ce
Diego Mercado <102191086+Diego2997@users.noreply.github.com>
2023-03-13 21:54:37 -0300

    Update ejercicio7.js

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,7 +1,7 @@
 // Escriba un script que muestre la tabla de multiplicar de un número ingresado por pantalla, la creación de la tabla debe ser realizada con una función y mostrar solo los resultados del 1 al 10 del número elegido por el usuario.
 
 function tablaMultiplicar(num) {
-  for (var i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 10; i++) {
     tablaMultiplicar = num * i;
     document.write(num + "x" + i + "=" + tablaMultiplicar + "<br>");
   }
